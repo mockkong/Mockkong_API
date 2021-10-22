4126019f882c9141ac8286b310c9583b2cf35378
DavidKimDY <keayoub@gmail.com>
2021-10-22 23:43:51 +0900

    10.22

--- application.py
+++ application.py
@@ -1,20 +1,6 @@
-from flask import Flask, request, flash, jsonify, Blueprint, Response
+from web.app import create_app
 
-
-app = Flask(__name__)
-
-
-@app.route("/signin", methods=["GET", "POST", "OPTIONS"])
-def home():
-    if request.method == "POST":
-        print(request.form)
-        print(dict(request.form))
-        my_res = Response(jsonify(request.form))
-        my_res.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
-
-        return my_res
-    return Response("Flask")
-
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
