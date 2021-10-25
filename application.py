82bbde31eb0046f1a5ee917ec4dd002957745d13
DavidKimDY <keayoub@gmail.com>
2021-10-25 19:09:52 +0900

    Edit : CORS

--- application.py
+++ application.py
@@ -1,19 +1,18 @@
 from flask import Flask, request, flash, jsonify, Blueprint, Response
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/signin", methods=["GET", "POST", "OPTIONS"])
 def home():
     if request.method == "POST":
-        print(request.form)
-        print(dict(request.form))
-        my_res = Response(jsonify(request.form))
-        my_res.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
+        my_res = request.form
+        print(my_res)
 
         return my_res
-    return Response("Flask")
+    return "success"
 
 
 if __name__ == "__main__":
